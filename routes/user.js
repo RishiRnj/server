7865f4d1294b5ea83f9b3840a651194915a6ca95
RishiRnj <bsrem4hindu@gmail.com>
2025-05-14 14:11:46 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -705,7 +705,7 @@ router.post('/:id/follow/send-back-userSuggestions', protect, async (req, res) =
 
 //for dashboard statistics
 // Get total users, total beneficiaries, and total beneficiaries with fulfilled donation status, Total blood donor in one request
-router.get('/profile', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // Fetch total users count
     const totalUsers = await User.countDocuments();
